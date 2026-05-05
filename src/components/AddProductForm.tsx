c873dde8739ccaaeea6f44998c9ef0a6a82169ec
vinaypurbia <vinay.purbia@gmail.com>
2026-05-05 13:14:36 +0300

    Create AddProductForm.tsx

---
+++ src/components/AddProductForm.tsx
@@ -0,0 +1,33 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
+import { Layout } from './components/Layout';
+import { Home } from './pages/Home';
+import { Catalog } from './pages/Catalog';
+import { ProductDetail } from './pages/ProductDetail';
+import { OrderSummary } from './pages/OrderSummary';
+import AddProductForm from './components/AddProductForm';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <CartProvider>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Catalog />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
+            <Route path="/order" element={<OrderSummary />} />
+            <Route path="/add-product" element={<AddProductForm />} />
+            {/* Catch-all to redirect to Home */}
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </Layout>
+      </CartProvider>
+    </BrowserRouter>
+  );
+}
