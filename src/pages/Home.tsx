71e7d3e4deed655ac49178e5bc126cb8dccb0c9f
vinaypurbia <vinay.purbia@gmail.com>
2026-05-06 16:22:37 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -124,7 +124,7 @@ export function Home() {
               <div className="relative z-10 flex flex-col h-full items-start">
                 <CheckCircle className="w-6 h-6 mb-4 text-black" />
                 <h3 className="font-black text-lg mb-2 uppercase">Confirm</h3>
-                <p className="text-xs font-bold text-white/80 uppercase tracking-wide">We reply with payment instructions & delivery.</p>
+                <p className="text-xs font-bold text-black/80 uppercase tracking-wide">We reply with payment instructions & delivery.</p>
               </div>
             </div>
           </div>
